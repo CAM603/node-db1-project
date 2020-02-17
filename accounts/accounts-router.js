86b4bd88c63400cd10420f4848908d098ccbce21
CAM603 <cameronhawley@comcast.net>
2020-02-17 16:32:27 -0500

    ADD delete account

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -70,4 +70,18 @@ router.put('/:id', (req, res) => {
         })
 })
 
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    db('accounts')
+        .where({id})
+        .delete()
+        .then(count => {
+            res.status(200).json(count)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: 'Failed to delete account' })
+        })
+})
+
 module.exports = router;
