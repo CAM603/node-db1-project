d940ce0f80cb6fe990e5e05cc426c32cef0b881e
CAM603 <cameronhawley@comcast.net>
2020-02-17 16:21:37 -0500

    ADD get and post for account

---
+++ accounts/accounts-router.js
@@ -0,0 +1,60 @@
+const express = require('express');
+
+const db = require('../data/dbConfig');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    db('accounts')
+        .then(accounts => {
+            res.status(200).json(accounts)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: 'Failed to get list of accounts' })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    db('accounts')
+        .where({ id: req.params.id })
+        .then(account => {
+            res.status(200).json(account[0])
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: 'Failed to retrieve account' })
+        })
+})
+// WITH .first()
+router.get('/:id', (req, res) => {
+    db('accounts')
+        .where({ id: req.params.id })
+        .first()
+        .then(account => {
+            res.status(200).json(account)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: 'Failed to retrieve account' })
+        })
+})
+
+router.post('/', (req, res) => {
+    db('accounts')
+        .insert(req.body, 'id')
+        .then(ids => {
+            db('accounts')
+                // .where('id', ids[0])
+                .where({id: ids[0]})
+                .then(inserted => {
+                    res.status(201).json(inserted)
+                })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: 'Failed adding account'})
+        })
+})
+
+module.exports = router;
